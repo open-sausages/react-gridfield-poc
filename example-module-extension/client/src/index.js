dd25ec8a489fc70721d2e0a2189b876910f82da4
Aaron Carlino <unclecheese@leftandmain.com>
2018-06-28 10:33:32 +1200

    Initial commit of react gridfield POC

--- example-module-extension/client/src/index.js
+++
@@ -1,8 +0,0 @@
-import Injector, { injectGraphql } from 'lib/Injector';
-Injector.transform(
-  'noteslist-query-extension',
-  (updater) => {
-
-  },
-  { after: ['noteslist-graphql', 'notesaddform-graphql'] }
-);
